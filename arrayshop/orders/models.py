d04095cfe287c33a0bbe803e2d3438655734f6dd
Nail <nailkalimov@gmail.com>
2024-12-06 12:17:37 +0400

    add coupons in orders, payments and admin panel

--- arrayshop/orders/models.py
+++ arrayshop/orders/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from shop.models import Product
 from django.conf import settings
+from decimal import Decimal
+from django.core.validators import MinValueValidator, MaxValueValidator
+from coupons.models import Coupon
 
 # Create your models here.
+
 
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
@@ -15,21 +19,23 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     stripe_id = models.CharField(max_length=250, blank=True)
+    coupon = models.ForeignKey(Coupon,
+                               related_name='orders',
+                               null=True,
+                               blank=True,
+                               on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0,
+                                   validators=[MinValueValidator(0),
+                                               MaxValueValidator(20)])
 
     class Meta:
         ordering = ['-created']
-        indexes=[
+        indexes = [
             models.Index(fields=['-created'])
         ]
 
-
     def __str__(self) -> str:
         return f'Order {self.id}'
-    
-
-    def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
-    
 
     def get_stripe_url(self):
         if not self.stripe_id:
@@ -39,21 +45,34 @@ class Order(models.Model):
         else:
             path = '/'
         return f'https://dashboard.stripe.com{path}payments/{self.stripe_id}'
-    
+
+    def get_total_cost_before_discount(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
+
+    def get_total_cost(self):
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
+
+
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, 
+    order = models.ForeignKey(Order,
                               related_name='items',
                               on_delete=models.CASCADE)
     product = models.ForeignKey(Product,
                                 related_name='order_items',
                                 on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, 
+    price = models.DecimalField(max_digits=10,
                                 decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self) -> str:
         return str(self.id)
-
 
     def get_cost(self):
         return self.price * self.quantity
