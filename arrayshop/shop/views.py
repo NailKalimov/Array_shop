82e9a8ecd3b1e9a859fa89e848ae78ddd7f8e050
Nail <nailkalimov@gmail.com>
2024-12-08 20:37:22 +0400

    Added recommendations

--- arrayshop/shop/views.py
+++ arrayshop/shop/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
+from .recommender import Recommender
 
 # Create your views here.
+
+
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
@@ -12,14 +15,17 @@ def product_list(request, category_slug=None):
         products = products.filter(category=category)
     return render(request, template_name='shop/product/list.html',
                   context={'category': category,
-                   'categories': categories,
-                   'products': products})
+                           'categories': categories,
+                           'products': products})
 
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
-    return render(request, 
+    r = Recommender()
+    recommended_products = r.suggest_products_for([product], 4)
+    return render(request,
                   template_name='shop/product/detail.html',
                   context={'product': product,
-                   'cart_product_form': cart_product_form})
+                           'cart_product_form': cart_product_form,
+                           'recommended_products': recommended_products})
